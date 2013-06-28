09ac0dc186539b3954a0d18ab0160f7b5573ea80
Ken Johnson <johnsk16@gmail.com>
2013-06-28 03:54:40 -0500

    Added CTN's above 20 Added slaying dice Made it pretty

--- combat.js
+++ combat.js
@@ -2,276 +2,321 @@ on("chat:message", function(msg)
 {
 	var debug = true;
 	
-	// Defense roll (DEF)
-	if(msg.type == "api" && msg.content.indexOf("!def") !== -1) 
+	if (msg.type == "api")
 	{
-		var character = getCharacter(msg.who);
+		var character = null;
+		var checkValue = 0;
+		var summation = "";
+		var message = "";
+		var attributes = null;
+		var BOD = null;
+		var AV = null;
+		var MOB = null;
+		var DX = null;
+		var bonus = 0;
+		var WB = null;
+		var marksman = null;
+		var MND = null;
+		var rollResults = null;
+		var output = "";
 
-		if (character)
+		// Defense roll (DEF)
+		if(msg.content.indexOf("!def") !== -1) 
 		{
-			var rollValue = randomInteger(20);
-			var checkValue = 0;
-			var summation = "";
-			var message = msg.content.replace("!def", "").toLowerCase();
-			var attributes = getAttributes(character);
+			character = getCharacter(msg.who);
 
-			var BOD = getAttributeValue(attributes, "BOD");
-			var CO = getAttributeValue(attributes, "CO");
-			var AV = getAttributeValue(attributes, "AV");
-			var ritualofscars = getAttributeValue(attributes, "Ritual of Scars");
-
-			checkValue = BOD + CO + AV;
-			summation = "BOD(" + BOD + ") + CO(" + CO + ") + AV(" + AV + ")";
-
-			if (ritualofscars)
+			if (character)
 			{
-				checkValue += (ritualofscars * 2);
-				summation += " + Ritual of Scars(" + (ritualofscars * 2) + ")";
-			}
+				checkValue = 0;
+				summation = "";
+				message = msg.content.replace("!def", "").toLowerCase();
+				attributes = getAttributes(character);
 
-			// Test to see if DEF exists, if not, create it.
-			var oDEF = getAttribute(attributes, "DEF");
+				BOD = getAttributeValue(attributes, "BOD");
+				var CO = getAttributeValue(attributes, "CO");
+				AV = getAttributeValue(attributes, "AV");
+				var ritualofscars = getAttributeValue(attributes, "Ritual of Scars");
 
-			if (!oDEF)
-				oDEF = createAttribute(character, "DEF");
+				checkValue = BOD + CO + AV;
+				summation = "BOD(" + BOD + ") + CO(" + CO + ") + AV(" + AV + ")";
 
-			// Update DEF
-			setAttributeValue(oDEF, checkValue);
-
-			// Leftover data to handle
-			if (message.length > 0)
-			{
-				var bonus = tallyLeftovers(message);
-				
-				if (bonus)
+				if (ritualofscars)
 				{
-					checkValue += bonus;
-					summation += " + (" + bonus + ")";
-					message = "";
+					checkValue += (ritualofscars * 2);
+					summation += " + Ritual of Scars(" + (ritualofscars * 2) + ")";
 				}
-			}
-			sendChat(msg.who, "/em attempts to defend against an attack ... " + rollValue + " vs " + checkValue + " [" + summation + "] attempt is a " + (rollValue <= checkValue ? (rollValue == 1 ? "CRITICAL " : "") + "SUCCESS!" : "FAILURE!"));
-		}
-		else
-			sendChat(msg.who, "/em tried to defend but they aren't even a character!");
-	}
-	// Melee attack (MAT)
-	if(msg.type == "api" && msg.content.indexOf("!melee") !== -1) 
-	{
-		var character = getCharacter(msg.who);
 
-		if (character)
+				// Test to see if DEF exists, if not, create it.
+				var oDEF = getAttribute(attributes, "DEF");
+
+				if (!oDEF)
+					oDEF = createAttribute(character, "DEF");
+
+				// Update DEF
+				setAttributeValue(oDEF, checkValue);
+
+				// Leftover data to handle
+				if (message.length > 0)
+				{
+					bonus = tallyLeftovers(message);
+					
+					if (bonus)
+					{
+						checkValue += bonus;
+						summation += " + (" + bonus + ")";
+						message = "";
+					}
+				}
+
+				rollResults = roll(checkValue);
+				output = rollFormat(msg.who, checkValue, rollResults);
+
+				sendChat(msg.who, "/em attempts to defend against an attack ... Check(" + checkValue + ")[" + summation + "] results in " + output + ", it's a " + (rollResults.total > 0 ? "SUCCESS" : (rollResults.dice[0] == 20 ? "***FUMBLE***" : "FAILURE")) + (rollResults.total > 0 ? (" For " + (rollResults.total + rollResults.slayingTotal) + " less damage!") : ""));
+			}
+			else
+				sendChat(msg.who, "/em tried to defend but they aren't even a character!");
+		}
+		// Melee attack (MAT)
+		if(msg.type == "api" && msg.content.indexOf("!melee") !== -1) 
 		{
-			var rollValue = randomInteger(20);
-			var checkValue = 0;
-			var summation = "";
-			var message = msg.content.replace("!melee", "").toLowerCase();
-			var attributes = getAttributes(character);
+			character = getCharacter(msg.who);
 
-			var BOD = getAttributeValue(attributes, "BOD");
-			var ST = getAttributeValue(attributes, "ST");
-			var WB = getAttributeValue(attributes, "WB");
-			var closecombat = getAttributeValue(attributes, "Close Combat");
-
-			checkValue = BOD + ST + WB;
-			summation = "BOD(" + BOD + ") + ST(" + ST + ") + WB(" + WB + ")";
-
-			if (closecombat)
+			if (character)
 			{
-				checkValue += closecombat;
-				summation += " + Close Combat(" + closecombat + ")";
-			}
+				rollValue = randomInteger(20);
+				checkValue = 0;
+				summation = "";
+				message = msg.content.replace("!melee", "").toLowerCase();
+				attributes = getAttributes(character);
 
-			// Test to see if DEF exists, if not, create it.
-			var oMAT = getAttribute(attributes, "MAT");
+				BOD = getAttributeValue(attributes, "BOD");
+				var ST = getAttributeValue(attributes, "ST");
+				WB = getAttributeValue(attributes, "WB");
+				var closecombat = getAttributeValue(attributes, "Close Combat");
 
-			if (!oMAT)
-				oMAT = createAttribute(character, "MAT");
+				checkValue = BOD + ST + WB;
+				summation = "BOD(" + BOD + ") + ST(" + ST + ") + WB(" + WB + ")";
 
-			// Update DEF
-			setAttributeValue(oMAT, checkValue);
-
-			// Leftover data to handle
-			if (message.length > 0)
-			{
-				var bonus = tallyLeftovers(message);
-				
-				if (bonus)
+				if (closecombat)
 				{
-					checkValue += bonus;
-					summation += " + (" + bonus + ")";
-					message = "";
+					checkValue += closecombat;
+					summation += " + Close Combat(" + closecombat + ")";
 				}
-			}
-			sendChat(msg.who, "/em tries to do a melee attack ... " + rollValue + " vs " + checkValue + " [" + summation + "] attempt is a " + (rollValue <= checkValue ? (rollValue == 1 ? "CRITICAL " : "") + "SUCCESS!" : "FAILURE!"));
-		}
-		else
-			sendChat(msg.who, "/em tried to attack but they aren't even a character!");
-	}
-	// Ranged attack (RAT)
-	if(msg.type == "api" && msg.content.indexOf("!ranged") !== -1) 
-	{
-		var character = getCharacter(msg.who);
 
-		if (character)
+				// Test to see if DEF exists, if not, create it.
+				var oMAT = getAttribute(attributes, "MAT");
+
+				if (!oMAT)
+					oMAT = createAttribute(character, "MAT");
+
+				// Update DEF
+				setAttributeValue(oMAT, checkValue);
+
+				// Leftover data to handle
+				if (message.length > 0)
+				{
+					bonus = tallyLeftovers(message);
+					
+					if (bonus)
+					{
+						checkValue += bonus;
+						summation += " + (" + bonus + ")";
+						message = "";
+					}
+				}
+
+				rollResults = roll(checkValue);
+				output = rollFormat(msg.who, checkValue, rollResults);
+
+				sendChat(msg.who, "/em tries to do a melee attack ... Check(" + checkValue + ")[" + summation + "] results in " + output + ", it's a " + (rollResults.total > 0 ? "SUCCESS" : (rollResults.dice[0] == 20 ? "***FUMBLE***" : "FAILURE")) + (rollResults.total > 0 ? (" For " + (rollResults.total + rollResults.slayingTotal) + " damage!") : ""));
+			}
+			else
+				sendChat(msg.who, "/em tried to attack but they aren't even a character!");
+		}
+		// Ranged attack (RAT)
+		if(msg.type == "api" && msg.content.indexOf("!ranged") !== -1) 
 		{
-			var rollValue = randomInteger(20);
-			var checkValue = 0;
-			var summation = "";
-			var message = msg.content.replace("!ranged", "").toLowerCase();
-			var attributes = getAttributes(character);
+			character = getCharacter(msg.who);
 
-			var MOB = getAttributeValue(attributes, "MOB");
-			var DX = getAttributeValue(attributes, "DX");
-			var WB = getAttributeValue(attributes, "WB");
-			var marksman = getAttributeValue(attributes, "Marksman");
-
-			checkValue = MOB + DX + WB;
-			summation = "MOB(" + MOB + ") + DX(" + DX + ") + WB(" + WB + ")";
-
-			if (marksman)
+			if (character)
 			{
-				checkValue += marksman;
-				summation += " + Marksman(" + marksman + ")";
-			}
+				checkValue = 0;
+				summation = "";
+				message = msg.content.replace("!ranged", "").toLowerCase();
+				attributes = getAttributes(character);
 
-			// Test to see if RAT exists, if not, create it.
-			var oRAT = getAttribute(attributes, "RAT");
+				MOB = getAttributeValue(attributes, "MOB");
+				DX = getAttributeValue(attributes, "DX");
+				WB = getAttributeValue(attributes, "WB");
+				marksman = getAttributeValue(attributes, "Marksman");
 
-			if (!oRAT)
-				oRAT = createAttribute(character, "RAT");
+				checkValue = MOB + DX + WB;
+				summation = "MOB(" + MOB + ") + DX(" + DX + ") + WB(" + WB + ")";
 
-			// Update DEF
-			setAttributeValue(oRAT, checkValue);
-
-			// Leftover data to handle
-			if (message.length > 0)
-			{
-				var bonus = tallyLeftovers(message);
-				
-				if (bonus)
+				if (marksman)
 				{
-					checkValue += bonus;
-					summation += " + (" + bonus + ")";
-					message = "";
+					checkValue += marksman;
+					summation += " + Marksman(" + marksman + ")";
 				}
-			}
-			sendChat(msg.who, "/em tries to do a ranged attack ... " + rollValue + " vs " + checkValue + " [" + summation + "] attempt is a " + (rollValue <= checkValue ? (rollValue == 1 ? "CRITICAL " : "") + "SUCCESS!" : "FAILURE!"));
-		}
-		else
-			sendChat(msg.who, "/em tried to attack but they aren't even a character!");
-	}
-	// Spell casting (SPC)
-	if(msg.type == "api" && msg.content.indexOf("!spell") !== -1) 
-	{
-		var character = getCharacter(msg.who);
 
-		if (character)
+				// Test to see if RAT exists, if not, create it.
+				var oRAT = getAttribute(attributes, "RAT");
+
+				if (!oRAT)
+					oRAT = createAttribute(character, "RAT");
+
+				// Update DEF
+				setAttributeValue(oRAT, checkValue);
+
+				// Leftover data to handle
+				if (message.length > 0)
+				{
+					bonus = tallyLeftovers(message);
+					
+					if (bonus)
+					{
+						checkValue += bonus;
+						summation += " + (" + bonus + ")";
+						message = "";
+					}
+				}
+
+				rollResults = roll(checkValue);
+				output = rollFormat(msg.who, checkValue, rollResults);
+
+				sendChat(msg.who, "/em tries to do a ranged attack ... Check(" + checkValue + ")[" + summation + "] results in " + output + ", it's a " + (rollResults.total > 0 ? "SUCCESS" : (rollResults.dice[0] == 20 ? "***FUMBLE***" : "FAILURE")) + (rollResults.total > 0 ? (" For " + (rollResults.total + rollResults.slayingTotal) + " damage!") : ""));
+			}
+			else
+				sendChat(msg.who, "/em tried to attack but they aren't even a character!");
+		}
+		// Spell casting (SPC)
+		if(msg.type == "api" && msg.content.indexOf("!spell") !== -1) 
 		{
-			var rollValue = randomInteger(20);
-			var checkValue = 0;
-			var summation = "";
-			var message = msg.content.replace("!spell", "").toLowerCase();
-			var attributes = getAttributes(character);
+			character = getCharacter(msg.who);
 
-			var MND = getAttributeValue(attributes, "MND");
-			var AU = getAttributeValue(attributes, "AU");
-			var AV = getAttributeValue(attributes, "AV");
-			var SPCMOD = getAttributeValue(attributes, "SPC%");
-
-			checkValue = MND + AU - AV;
-			summation = "MND(" + MND + ") + AU(" + AU + ") - AV(" + AV + ")";
-
-			if (SPCMOD)
+			if (character)
 			{
-				checkValue += SPCMOD;
-				summation += " + Bonus(" + SPCMOD + ")";
-			}
-			
-			// Test to see if RAT exists, if not, create it.
-			var oSPC = getAttribute(attributes, "SPC");
+				checkValue = 0;
+				summation = "";
+				message = msg.content.replace("!spell", "").toLowerCase();
+				attributes = getAttributes(character);
 
-			if (!oSPC)
-				oSPC = createAttribute(character, "SPC");
+				MND = getAttributeValue(attributes, "MND");
+				var AU = getAttributeValue(attributes, "AU");
+				AV = getAttributeValue(attributes, "AV");
+				var SPCMOD = getAttributeValue(attributes, "SPC%");
 
-			// Update DEF
-			setAttributeValue(oSPC, checkValue);
+				checkValue = MND + AU - AV;
+				summation = "MND(" + MND + ") + AU(" + AU + ") - AV(" + AV + ")";
 
-			// Leftover data to handle
-			if (message.length > 0)
-			{
-				var bonus = tallyLeftovers(message);
-				
-				if (bonus)
+				if (SPCMOD)
 				{
-					checkValue += bonus;
-					summation += " + (" + bonus + ")";
-					message = "";
+					checkValue += SPCMOD;
+					summation += " + Bonus(" + SPCMOD + ")";
 				}
-			}
-			sendChat(msg.who, "/em tries to cast a spell ... " + rollValue + " vs " + checkValue + " [" + summation + "] attempt is a " + (rollValue <= checkValue ? (rollValue == 1 ? "CRITICAL " : "") + "SUCCESS!" : "FAILURE!"));
-		}
-		else
-			sendChat(msg.who, "/em tried to cast a spell but they aren't even a character!");
-	}
-	// Targeted Spell Casting (TSC)
-	if(msg.type == "api" && msg.content.indexOf("!tspell") !== -1) 
-	{
-		var character = getCharacter(msg.who);
+				
+				// Test to see if RAT exists, if not, create it.
+				var oSPC = getAttribute(attributes, "SPC");
 
-		if (character)
+				if (!oSPC)
+					oSPC = createAttribute(character, "SPC");
+
+				// Update DEF
+				setAttributeValue(oSPC, checkValue);
+
+				// Leftover data to handle
+				if (message.length > 0)
+				{
+					bonus = tallyLeftovers(message);
+					
+					if (bonus)
+					{
+						checkValue += bonus;
+						summation += " + (" + bonus + ")";
+						message = "";
+					}
+				}
+
+				rollResults = roll(checkValue);
+				output = rollFormat(msg.who, checkValue, rollResults);
+
+				sendChat(msg.who, "/em tries to cast a spell ... Check(" + checkValue + ")[" + summation + "] results in " + output + ", it's a " + (rollResults.total > 0 ? "SUCCESS" : (rollResults.dice[0] == 20 ? "***FUMBLE***" : "FAILURE")) + (rollResults.total > 0 ? (" For " + (rollResults.total + rollResults.slayingTotal) + " damage!") : ""));
+			}
+			else
+				sendChat(msg.who, "/em tried to cast a spell but they aren't even a character!");
+		}
+		// Targeted Spell Casting (TSC)
+		if(msg.type == "api" && msg.content.indexOf("!tspell") !== -1) 
 		{
-			var rollValue = randomInteger(20);
-			var checkValue = 0;
-			var summation = "";
-			var message = msg.content.replace("!tspell", "").toLowerCase();
-			var attributes = getAttributes(character);
+			character = getCharacter(msg.who);
 
-			var MND = getAttributeValue(attributes, "MND");
-			var DX = getAttributeValue(attributes, "DX");
-			var AV = getAttributeValue(attributes, "AV");
-			var TSCMOD = getAttributeValue(attributes, "TSC%");
-			var marksman = getAttributeValue(attributes, "Marksman");
-
-			checkValue = MND + DX - AV;
-			summation = "MND(" + MND + ") + DX(" + DX + ") - AV(" + AV + ")";
-
-			if (TSCMOD)
+			if (character)
 			{
-				checkValue += TSCMOD;
-				summation += " + Bonus(" + TSCMOD + ")";
-			}
-			
-			if (marksman)
-			{
-				checkValue += marksman;
-				summation += " + Marksman(" + marksman + ")";
-			}
+				checkValue = 0;
+				summation = [];
+				message = msg.content.replace("!tspell", "").toLowerCase();
+				attributes = getAttributes(character);
 
-			// Test to see if RAT exists, if not, create it.
-			var oTSC = getAttribute(attributes, "TSC");
+				MND = getAttributeValue(attributes, "MND");
+				DX = getAttributeValue(attributes, "DX");
+				AV = getAttributeValue(attributes, "AV");
+				var TSCMOD = getAttributeValue(attributes, "TSC%");
+				marksman = getAttributeValue(attributes, "Marksman");
 
-			if (!oTSC)
-				oTSC = createAttribute(character, "TSC");
+				checkValue = MND + DX - AV;
+				summation.push(["MND", MND, "+"]);
+				summation.push(["DX", DX, "+"]);
+				summation.push(["AV", AV, "-"]);
+				//summation = "MND(" + MND + ") + DX(" + DX + ") - AV(" + AV + ")";
 
-			// Update DEF
-			setAttributeValue(oTSC, checkValue);
-
-			// Leftover data to handle
-			if (message.length > 0)
-			{
-				var bonus = tallyLeftovers(message);
-				
-				if (bonus)
+				if (TSCMOD)
 				{
-					checkValue += bonus;
-					summation += " + (" + bonus + ")";
-					message = "";
+					checkValue += TSCMOD;
+					summation.push(["Bonus", TSCMOD, "+"]);
+					//summation += " + Bonus(" + TSCMOD + ")";
 				}
+				
+				if (marksman)
+				{
+					checkValue += marksman;
+					summation.push(["Marksman", marksman, "+"]);
+					//summation += " + Marksman(" + marksman + ")";
+				}
+
+				// Test to see if RAT exists, if not, create it.
+				var oTSC = getAttribute(attributes, "TSC");
+
+				if (!oTSC)
+					oTSC = createAttribute(character, "TSC");
+
+				// Update DEF
+				setAttributeValue(oTSC, checkValue);
+
+				// Leftover data to handle
+				if (message.length > 0)
+				{
+					bonus = tallyLeftovers(message);
+					
+					if (bonus)
+					{
+						checkValue += bonus;
+						summation.push(["", bonus, "+"]);
+						//summation += " + (" + bonus + ")";
+						message = "";
+					}
+				}
+
+				rollResults = roll(checkValue);
+				output = ctnFormat(summation);
+				output = rollFormat(msg.who, "I cast a targeted spell", output, checkValue, rollResults);
+
+				sendChat(msg.who, "/direct " + output);
+				//sendChat(msg.who, "/em tries to cast a spell on a target ... Check(" + checkValue + ")[" + summation + "] results in " + output + ", it's a " + (rollResults.total > 0 ? "SUCCESS" : (rollResults.dice[0] == 20 ? "***FUMBLE***" : "FAILURE")) + (rollResults.total > 0 ? (" For " + (rollResults.total + rollResults.slayingTotal) + " damage!") : ""));
+				//sendChat(msg.who, "/em tries to cast a spell on a target and " + (rollResults.total > 0 ? "SUCCEEDS" : (rollResults.dice[0] == 20 ? "FUMBLES" : "FAILS")) + (rollResults.total > 0 ? (" for " + (rollResults.total + rollResults.slayingTotal) + " damage!") : ""));
 			}
-			sendChat(msg.who, "/em tries to cast a spell on a target ... " + rollValue + " vs " + checkValue + " [" + summation + "] attempt is a " + (rollValue <= checkValue ? (rollValue == 1 ? "CRITICAL " : "") + "SUCCESS!" : "FAILURE!"));
+			else
+				sendChat(msg.who, "/em tried to cast a spell but they aren't even a character!");
 		}
-		else
-			sendChat(msg.who, "/em tried to cast a spell but they aren't even a character!");
+
 	}
 });
--- utility.js
+++ utility.js
@@ -80,11 +80,17 @@ function getAttributeValue(attributes, attribute)
 
 function getAttributes(character)
 {
-	var attributes = findObjs({
-		_type: "attribute",
-		_characterid: character.get('_id')
-	});
-	return attributes;
+	var rtn = null;
+
+	if (character)
+	{
+		rtn = findObjs({
+			_type: "attribute",
+			_characterid: character.get('_id')
+		});
+	}
+
+	return rtn;
 }
 
 function getInitiative(token)
@@ -92,7 +98,7 @@ function getInitiative(token)
 	var initiative = 0;
 	// check to see if character
 	var represents = token.get("_represents");
-	if (represents && represents != "")
+	if (represents && represents !== "")
 	{
 		var character = getObj("character", represents);
 		var attributes = getAttributes(character);
@@ -109,7 +115,7 @@ function getInitiative(token)
 	}
 	else
 	{
-		initiative = parseInt(token.get("bar3_value"), 10)
+		initiative = parseInt(token.get("bar3_value"), 10);
 	}
 	if (isNaN(initiative))
 		initiative = 0;
@@ -147,6 +153,177 @@ function tallyLeftovers(msg)
 
 		if (bonus > 0)
 			rtn = bonus;
+	}
+
+	return rtn;
+}
+
+// Roll against a CTN and auto max damage/result, a null is a fumble
+function roll(ctn)
+{
+	rtn = null;
+
+	if (+ctn > 0)
+	{
+		var dice = Math.ceil(+ctn / 20);
+		var origRolls = [];
+		var cloneRolls = [];
+		var slayingRolls = [];
+		var ctns = [];
+		var count = 0;
+		var slaying = 0;
+		
+		// Get all dice roll values and set all die CTN's
+		for (var i = 0; i < dice; i++)
+		{
+			origRolls[i] = randomInteger(20);
+			ctns[i] = 20;
+
+			if (i == (dice - 1))
+			{
+				if (ctn % 20 !== 0)
+					ctns[i] = ctn % 20;
+			}
+		}
+
+		// Test for slaying dice
+		if (origRolls[0] == 1)
+		{
+			var tmpRoll = randomInteger(20);
+			i = 0;
+			// Keep rolling on coups
+			while (tmpRoll == 1)
+			{
+				slaying += ctns[0];
+				slayingRolls[i] = tmpRoll;
+				tmpRoll = randomInteger(20);
+				i++;
+			}
+
+			slayingRolls[i] = tmpRoll;
+			if (tmpRoll <= ctns[0])
+				slaying += tmpRoll;
+		}
+
+		// Test for fumble
+		if (origRolls[0] == 20)
+			rtn = {total: 0, checks: ctns, dice: origRolls, slayingTotal: 0, slayingDice: slayingRolls};
+		else
+		{
+			cloneRolls = origRolls.slice();
+			// Re-arrange rolls
+			cloneRolls.sort(function (a, b){
+				if (a == 1 && b != 1)
+					return -1;
+				if (b == 1 && a != 1)
+					return 1;
+				if (a > b)
+					return -1;
+				if (a < b)
+					return 1;
+				return 0;
+			});
+
+			for (i = 0; i < dice; i++)
+			{
+				if (cloneRolls[i] <= ctns[i])
+					count += cloneRolls[i] == 1 ? ctns[i] : cloneRolls[i];
+			}
+
+			rtn = {total: count, checks: ctns, dice: cloneRolls, slayingTotal: slaying, slayingDice: slayingRolls};
+
+		}
+	}
+
+	return rtn;
+}
+
+function ctnFormat(ctnData)
+{
+	var rtn = null;
+
+	if (ctnData)
+	{
+		var	color1 = "background-color: #ABFFA3;";
+		var color2 = "background-color: #FFB8B8;";
+		var plus = "background-color: #ABFFA3;";
+		var minus = "background-color: #FFB8B8;";
+		var ctn = "background-color: #E3A6FF;";
+
+		var norm = "padding: 1px 2px 1px 2px; -webkit-border-radius: 3px; border: 1px solid #555555;";
+		var i = 0;
+
+		rtn = "<table style='font-size: 0.875em; border-collapse: separate; border-spacing: 2px; font-weight: bold;'><tr><td style='" + norm + ctn + "'>CTN</td><td> = </td>";
+
+		for (i = 0; i < ctnData.length; i++)
+		{
+			if (i !== 0)
+				rtn += "<td style='" + "'>" + ctnData[i][2] + "</td>";
+			rtn += "<td style='" + norm + (ctnData[i][2] === "+" ? plus : minus) + "'>" + ctnData[i][0] + "(" + ctnData[i][1] + ")" + "</td>";
+		}
+
+		rtn += "</tr></table>";
+	}
+
+	return rtn;
+}
+
+function rollFormat(who, msg, ctnData, ctn, rollData)
+{
+	var rtn = null;
+
+	if (rollData)
+	{
+		var miss = "background-color: #FFB8B8;";
+		var hit = "background-color: #C4C4C4;";
+		var coup = "background-color: #ABFFA3;";
+		var fumble = "background-color: #E3A6FF;";
+		var outside = "padding: 4px; -webkit-border-radius: 4px; border: 1px solid #555555;";
+		var i = 0;
+		var j = 0;
+
+		rtn = "<span>" + msg + " and " + (rollData.total > 0 ? "deal" : (rollData.dice[0] == 20 ? "<span style='font-weight: bold';>FUMBLE</span>" : "<span style='font-weight: bold';>MISS</span>")) + (rollData.total > 0 ? (" <span style='font-weight: bold';>" + (rollData.total + rollData.slayingTotal) + "</span> damage!") : "") + "</span>";
+		rtn += ctnData;
+		rtn += "<table style='margin=top: 7px; border-collapse: separate; border-spacing: 2px; font-weight: bold;'><tr>";
+
+		for(i = 0; i < rollData.checks.length; i++)
+		{
+			if (i === 0 && rollData.total === 0 && rollData.dice[i] === 20)
+			{
+				rtn += "<td style='" + fumble + outside + "'>";
+			}
+			else if (rollData.dice[i] <= rollData.checks[i])
+			{
+				if (rollData.dice[i] == 1)
+					rtn += "<td style='" + coup + outside + "'>";
+				else
+					rtn += "<td style='" + hit + outside + "'>";
+			}
+			else
+				rtn += "<td style='" + miss + outside + "'>";
+
+			rtn += (rollData.dice[i] < 10 ? "&nbsp;&nbsp;" + rollData.dice[i] : rollData.dice[i]) + "<span style=' font-weight: normal;'> vs </span>" + (rollData.checks[i] < 10 ? "&nbsp;&nbsp;" + rollData.checks[i] : rollData.checks[i]) + "</td>";
+
+			if (i === 0 && rollData.slayingDice.length > 0)
+			{
+				for (j = 0; j < rollData.slayingDice.length; j++)
+				{
+					if (rollData.slayingDice[j] <= rollData.checks[i])
+					{
+						if (rollData.slayingDice[j] == 1)
+							rtn += "<td style='" + coup + outside + "'>S ";
+						else
+							rtn += "<td style='" + hit + outside + "'>S ";
+					}
+					else
+						rtn += "<td style='" + miss + outside + "'>S ";
+
+					rtn += (rollData.slayingDice[i] < 10 ? "&nbsp;&nbsp;" + rollData.slayingDice[i] : rollData.slayingDice[i]) + "<span style=' font-weight: normal;'> vs </span>" + (rollData.checks[i] < 10 ? "&nbsp;&nbsp;" + rollData.checks[i] : rollData.checks[i]) + "</td>";
+				}
+			}
+
+		}
+		rtn += "</tr></table>";
 	}
 
 	return rtn;
